13be2d5011e2f44673b8fb0320adba609aab2115
zjypls <61042663+zjypls@users.noreply.github.com>
2023-06-08 10:29:33 +0800

    update Tetris

--- Tetris/Tetris.cpp
+++ Tetris/Tetris.cpp
@@ -2,12 +2,25 @@
 // Created by 32725 on 2023/6/6.
 //
 #include <QPainter>
+#include <QTimer>
+#include <QKeyEvent>
+#include <QRandomGenerator>
 #include "Tetris.h"
-
+static QRandomGenerator* generator=QRandomGenerator::global();
 zTetris::zTetris(QWidget *parent) : QWidget(parent){
-	this->resize(column * blockSize, row * blockSize);
-	std::memset(status, true, sizeof(status));
+	this->resize(column * (blockSize)+distance, row * (blockSize)+distance);
+	memset(status,empty,sizeof(status));
 	painter=new QPainter();
+	shape=zShape::GenerateShape();
+	static auto* PainterTimer=new QTimer(this);
+	connect(PainterTimer,&QTimer::timeout,[this](){
+		if(!TryFall()){
+			CheckDelete();
+			shape=zShape::GenerateShape();
+		}
+		update();
+	});
+	PainterTimer->start(500);
 	update();
 }
 
@@ -18,7 +31,9 @@ zTetris::~zTetris() {
 void zTetris::paintEvent(QPaintEvent *event) {
 	painter->begin(this);
 	painter->setPen(Qt::red);
+	painter->setBrush(Qt::red);
 	QWidget::paintEvent(event);
+	shape.Draw(painter);
 	for (int i = 0; i < column; ++i) {
 		for (int j = 0; j < row; ++j) {
 			if (status[i][j]) {
@@ -29,6 +44,91 @@ void zTetris::paintEvent(QPaintEvent *event) {
 	painter->end();
 }
 
+
+
 void zTetris::keyPressEvent(QKeyEvent *event) {
 	QWidget::keyPressEvent(event);
+	switch (event->key()) {
+		case Qt::Key_W:
+		case Qt::Key_Up:
+			shape.Rotate();
+			break;
+		case Qt::Key_A:
+		case Qt::Key_Left:
+			shape.Move(-1, status);
+			break;
+		case Qt::Key_D:
+		case Qt::Key_Right:
+			shape.Move(1, status);
+			break;
+		case Qt::Key_S:
+		case Qt::Key_Down:
+			TryFall();
+			break;
+		case Qt::Key_Space:
+			while(TryFall());
+			break;
+	}
+	update();
+}
+
+bool zTetris::TryFall() {
+	static ivec2 pos[4];
+	std::copy(shape.pos,shape.pos+4,pos);
+	for(auto & po : pos) {
+		po.y++;
+		if(po.y>=row||status[po.x][po.y]==full){
+			for(auto & p : shape.pos) {
+				status[p.x][p.y]=full;
+			}
+			return false;
+		}
+	}
+	std::copy(pos,pos+4,shape.pos);
+	return true;
+}
+
+void zTetris::CheckDelete() {
+	for (int i = 0; i < row; ++i) {
+		bool full=true;
+		for (auto & statu : status) {
+			if(statu[i]==empty){
+				full=false;
+				break;
+			}
+		}
+		if(full){
+			for (auto & statu : status) {
+				statu[i]=empty;
+			}
+			for (int j = i; j > 0; --j) {
+				for (auto & statu : status) {
+					statu[j]=statu[j-1];
+				}
+			}
+		}
+	}
+
+}
+
+zShape zShape::GenerateShape() {
+	auto type=static_cast<zShapeType>(generator->bounded(0,6));
+	//qDebug()<<test::shapeTypeToString(type);
+	return zShape(type);
+}
+
+QString test::shapeTypeToString(zShape::zShapeType type){
+	switch (type) {
+		#define caseType(x) case zShape::zShapeType::e##x: return #x
+		caseType(I);
+		caseType(O);
+		caseType(T);
+		caseType(S);
+		caseType(Z);
+		caseType(J);
+		caseType(L);
+		#undef caseType
+		default:
+			throw std::runtime_error("zShapeType error");
+	}
 }
--- Tetris/Tetris.h
+++ Tetris/Tetris.h
@@ -6,11 +6,116 @@
 #define SUMARY_TETRIS_H
 #include <QWidget>
 #include <QVector2D>
-
+#include <QVector>
+#include <QPainter>
+constexpr short empty=0;
+constexpr short full=1;
+constexpr int rotateBase=1;
 constexpr int column = 10;
 constexpr int row = 20;
 constexpr int blockSize = 40;
-constexpr int distance = 2;
+constexpr int distance = 4;
+
+struct ivec2{
+	int x,y;
+	ivec2(int x=0,int y=0):x(x),y(y){}
+	ivec2 operator+(const ivec2& other) const{
+		return {x+other.x,y+other.y};
+	}
+	ivec2 operator-(const ivec2& other) const{
+		return {x-other.x,y-other.y};
+	}
+	ivec2 operator+=(int other){
+		return {x+=other,y+=other};
+	}
+	ivec2 operator-=(int other){
+		return {x-=other,y-=other};
+	}
+	ivec2 operator+=(const ivec2& other){
+		return {x+=other.x,y+=other.y};
+	}
+	ivec2 operator-=(const ivec2& other){
+		return {x-=other.x,y-=other.y};
+	}
+	ivec2 operator*(ivec2 other) const{
+		return {x*other.x,y*other.y};
+	}
+	ivec2 operator~() const{
+		return {y,x};
+	}
+};
+
+
+struct zShape{
+	ivec2 pos[4]{};
+	enum class zShapeType{
+		eI=0,eO,eT,eS,eZ,eJ,eL
+	};
+	zShapeType Type;
+	explicit zShape(zShapeType type=zShapeType::eI):Type(type){
+		switch (type) {
+			case zShapeType::eI:
+				pos[0]={2,0};pos[1]={3,0};pos[2]={4,0};pos[3]={5,0};
+				break;
+			case zShapeType::eO:
+				pos[0]={3,0};pos[1]={4,0};pos[2]={3,1};pos[3]={4,1};
+				break;
+			case zShapeType::eT:
+				pos[0]={3,0};pos[1]={2,1};pos[2]={3,1};pos[3]={4,1};
+				break;
+			case zShapeType::eS:
+				pos[0]={3,0};pos[1]={4,0};pos[2]={2,1};pos[3]={3,1};
+				break;
+			case zShapeType::eZ:
+				pos[0]={2,0};pos[1]={3,0};pos[2]={3,1};pos[3]={4,1};
+				break;
+			case zShapeType::eJ:
+				pos[0]={2,0};pos[1]={2,1};pos[2]={3,1};pos[3]={4,1};
+				break;
+			case zShapeType::eL:
+				pos[0]={4,0};pos[1]={2,1};pos[2]={3,1};pos[3]={4,1};
+				break;
+			default:
+				throw std::runtime_error("zShapeType error");
+		}
+	}
+	void Draw(QPainter* painter){
+		for (auto & po : pos)
+			painter->drawRect(po.x*blockSize+distance,po.y*blockSize+distance,blockSize-distance,blockSize-distance);
+
+	}
+	void Rotate(){
+		//Todo : fix rotate
+		return;
+		if(Type==zShapeType::eO)
+			return;
+		static ivec2 temp[4]{};
+		std::copy(pos,pos+4,temp);
+		for (auto & po : temp){
+			po+=((~(po-pos[rotateBase]))*ivec2(1,-1));
+			if(po.x<0||po.x>=column||po.y<0||po.y>=row)
+				return;
+		}
+		std::copy(temp,temp+4,pos);
+	}
+	void Move(int dir,short status[column][row]){
+		static ivec2 temp[4]{};
+		std::copy(pos,pos+4,temp);
+		for (auto & po : temp){
+			po.x+=dir;
+			if(po.x<0||po.x>=column||status[po.x][po.y]==full)
+				return;
+		}
+		std::copy(temp,temp+4,pos);
+	}
+	static zShape GenerateShape();
+	inline auto& operator[](int index){
+		return pos[index];
+	}
+};
+namespace test{
+	QString shapeTypeToString(zShape::zShapeType type);
+}
 
 class zTetris:public QWidget {
 public:
@@ -19,10 +124,12 @@ public:
 protected:
 	void paintEvent(QPaintEvent *event) override;
 	void keyPressEvent(QKeyEvent *event) override;
+	bool TryFall();
+	void CheckDelete();
 private:
-	QVector2D index{0, 0};
-	bool status[column][row]{true};
+	short status[column][row]{};
 	QPainter* painter;
+	zShape shape{zShape::zShapeType::eI};
 };
 
 
